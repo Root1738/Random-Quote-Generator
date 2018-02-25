ef778d8dfda1d60f9a1ca84540641026f70c6b34
Tim Dalton <tim@Tims-MacBook-Pro.local>
2018-02-24 22:31:00 -0600

    added comments to code

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// refreshes window every 30 seconds displaying a new quote and random background;
 var intervalID = window.setInterval(printQuote, 30000);
 
 var randomQuoteIndex;
 var randomQuote;
 var outputHTML = '';
+// quote object, stores quote,source, and tag
 var quotes = [
   {
     quote: "If it wasn't hard everyone would do it, it's the hard that makes it great.",
@@ -47,23 +49,30 @@ var quotes = [
 
 
 function getRandomQuote() {
+  // generates a random number from 0 -> # of quotes - 1
   randomQuoteIndex = Math.floor(Math.random() * quotes.length);
   return quotes[randomQuoteIndex];
 }
 
 function printQuote() {
   setRandomColor();
+  // gets a random quote object
   randomQuote = getRandomQuote();
+  // builds output HTML
   outputHTML = '<p class="quote">' + randomQuote.quote + '</p>';
   outputHTML += '<p class="source">' + randomQuote.source + '</p>';
+  // replaces #quote-box html with newly built random quote object.
   document.getElementById('quote-box').innerHTML = outputHTML;
 }
 
 function setRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
+
+  // generates a random color by selecting 6 random letters/numbers and appending it to an empty hex value string.
   for (i = 0; i < 6; i += 1) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+  // sets the background to the newly generated color
   document.body.style.backgroundColor = color;
 }
